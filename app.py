1129f9fc27ca32e8b5cad1be15c3f54ca6dc1342
jeongyeon <dlwjddus0512@gmail.com>
2025-08-23 11:34:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
     }
     
     .outlet-card {
-        background: #2b2b2b;
+        background: navy;
         padding: 1.5rem;
         border-radius: 10px;
         box-shadow: 0 2px 10px rgba(0,0,0,0.1);
